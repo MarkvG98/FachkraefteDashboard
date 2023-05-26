6baa6da75dca944eb425fb5fedc3a060379401e4
Jannik Hübschmann <jannik@huebschmann-online.de>
2023-05-26 11:31:12 +0200

    Working Rest Call

--- main/py/test.py
+++ main/py/test.py
@@ -1,16 +1,18 @@
 import requests
 
-url = 'https://rest.arbeitsagentur.de/oauth/gettoken_cc'
-
-headers = {"Content-Type": "application/json; charset=utf-8"}
+client_id = "c003a37f-024f-462a-b36d-b001be4cd24a"
+client_secret = "32a39620-32b3-4307-9aa1-511e3d7f48a8"
+url = "https://rest.arbeitsagentur.de/oauth/gettoken_cc"
 
 data = {
-    "client_id": "c003a37f-024f-462a-b36d-b001be4cd24a",
-    "client_secret": "32a39620-32b3-4307-9aa1-511e3d7f48a8",
-    "grant_type": "client_credentials",
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "grant_type": "client_credentials"
 }
- 
 
-request = requests.post(url,headers=headers,json=data)
+response = requests.post(url, data=data)
+response_json = response.json()
 
-print(requests)
+token = response_json.get("access_token")
+
+print(token)
