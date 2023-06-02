9dfc1dd0d486ad13ea10cc850bc5d61175334130
Melvin Redeker <mail@melvinredeker.de>
2023-06-02 14:32:31 +0200

    postal codes

---
+++ main/py/get_postal_codes.py
@@ -0,0 +1,12 @@
+import json
+
+home_path = 'C:/Users/melvi/Desktop/FachkraefteDashboard/main/import/'
+
+f = open(home_path+'geo_dataset.json')
+  
+data = json.load(f)
+  
+for gemeinde in data['gemeinden']:
+    print(gemeinde['postleitzahl'])
+  
+f.close()
