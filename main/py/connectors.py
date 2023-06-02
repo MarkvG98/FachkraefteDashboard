7ff76193654ace7dee1fe7a7cb6a8a9607deee6a
Jannik Hübschmann <jannik@huebschmann-online.de>
2023-06-02 23:45:42 +0200

    Dataframe mit Arbeitsort

--- main/py/connectors.py
+++ main/py/connectors.py
@@ -1,9 +1,9 @@
 import requests
-import pandas as pd
 from requests import Response
 
 class RestApiConnector:
-    def getHTTPResponse(self) -> Response:
+
+    def getHTTPResponse(self,page: int) -> Response:
         client_id = "c003a37f-024f-462a-b36d-b001be4cd24a"
         client_secret = "32a39620-32b3-4307-9aa1-511e3d7f48a8"
         url = "https://rest.arbeitsagentur.de/oauth/gettoken_cc"
@@ -29,7 +29,7 @@ class RestApiConnector:
             "wo": "Berlin",
             "umkreis": "200",
             "arbeitszeit": "ho;mj",
-            "page": "1",
+            "page": page,
             "size": "100",
             "pav": "false"
         }
--- main/py/pandas/dataAnalysisTesting.py
+++ main/py/pandas/dataAnalysisTesting.py
@@ -3,32 +3,44 @@ from main.py.connectors import RestApiConnector
 import pandas as pd
 
 con = RestApiConnector()
-jobs = con.getHTTPResponse().json()
+# jobs = con.getHTTPResponse().json()
 
 def set_pandas_display_options() -> None:
     # Ref: https://stackoverflow.com/a/52432757/
     display = pd.options.display
 
     display.max_columns = 1000
-    display.max_rows = 1000
+    display.max_rows = 5000
     display.max_colwidth = 199
     display.width = 1000
 
 set_pandas_display_options()
 
-print(jobs)
+def getDictForPages(pagesToShow: int) -> []:
+    data = []
+    for index in range(pagesToShow):
+       jobsForPage = con.getHTTPResponse(index+1).json()
+       for entity in jobsForPage["stellenangebote"]:
+           dict = {
+               "beruf": entity.get("beruf", ""),
+               "titel": entity.get("titel", ""),
+               "refnr": entity.get("refnr", ""),
+               "arbeitgeber": entity.get("arbeitgeber", ""),
+               "eintrittsdatum": entity.get("eintrittsdatum", ""),
+               "entfernung": entity.get("entfernung", ""),
+               "plz": entity.get("arbeitsort","plz")
+           }
+           data.append(dict)
 
-data = []
+    return data
 
-for entity in jobs["stellenangebote"]:
-    dict ={
-        "beruf": entity.get("beruf", ""),
-        "titel": entity.get("titel", ""),
-        "refnr": entity.get("refnr", ""),
-        "arbeitgeber": entity.get("arbeitgeber", ""),
-        "eintrittsdatum": entity.get("eintrittsdatum", "")
-    }
-    data.append(dict)
+# print()
+
+data = getDictForPages(15)
+
+
+
 
 df_nach_stellenangeboten = pd.DataFrame.from_dict(data)
+df_nach_stellenangeboten.sort_values(by=["beruf"])
 print(df_nach_stellenangeboten)
