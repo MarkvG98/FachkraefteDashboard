09d54da5d1c215d6c15aa5d019b57ddaa3784dc4
Jannik Hübschmann <jannik@huebschmann-online.de>
2023-06-16 12:58:56 +0200

    Fertiger DF für Powerbi

--- main/py/pandas/dataAnalysisTesting.py
+++ main/py/pandas/dataAnalysisTesting.py
@@ -1,46 +1,64 @@
-
 from main.py.connectors import RestApiConnector
 import pandas as pd
+import json
 
 con = RestApiConnector()
+
+
 # jobs = con.getHTTPResponse().json()
 
 def set_pandas_display_options() -> None:
     # Ref: https://stackoverflow.com/a/52432757/
     display = pd.options.display
-
     display.max_columns = 1000
     display.max_rows = 5000
     display.max_colwidth = 199
     display.width = 1000
 
+
 set_pandas_display_options()
+
 
 def getDictForPages(pagesToShow: int) -> []:
     data = []
     for index in range(pagesToShow):
-       jobsForPage = con.getHTTPResponse(index+1).json()
-       for entity in jobsForPage["stellenangebote"]:
-           dict = {
-               "beruf": entity.get("beruf", ""),
-               "titel": entity.get("titel", ""),
-               "refnr": entity.get("refnr", ""),
-               "arbeitgeber": entity.get("arbeitgeber", ""),
-               "eintrittsdatum": entity.get("eintrittsdatum", ""),
-               "entfernung": entity.get("entfernung", ""),
-               "plz": entity.get("arbeitsort","plz")
-           }
-           data.append(dict)
-
+        jobsForPage = con.getHTTPResponse(index + 1).json()
+        for entity in jobsForPage["stellenangebote"]:
+            dict = {
+                "beruf": entity.get("beruf", None),
+                "titel": entity.get("titel", None),
+                "refnr": entity.get("refnr", None),
+                "arbeitgeber": entity.get("arbeitgeber", None),
+                "eintrittsdatum": entity.get("eintrittsdatum", None),
+                "entfernung": entity.get("arbeitsort", "").get("entfernung", None),
+                "plz": entity.get("arbeitsort", "").get("plz", None)
+            }
+            data.append(dict)
     return data
 
-# print()
+
+def getPostalData() -> []:
+    home_path = '../../import/geo_dataset.json'
+    data = []
+    f = open(home_path)
+    cityCodes = json.load(f)
+    for gemeinde in cityCodes["gemeinden"]:
+        dict = {
+            "id": gemeinde.get("id", None),
+            "plz": gemeinde.get("postleitzahl", None)
+        }
+        data.append(dict)
+    f.close()
+    return data
+
 
 data = getDictForPages(15)
+cityData = getPostalData()
 
-
-
-
+df_codes = pd.DataFrame.from_dict(cityData)
 df_nach_stellenangeboten = pd.DataFrame.from_dict(data)
 df_nach_stellenangeboten.sort_values(by=["beruf"])
+df_nach_stellenangeboten = pd.merge(df_nach_stellenangeboten,df_codes,how='inner',on=["plz","plz"])
 print(df_nach_stellenangeboten)
+
+#print(df_codes)
