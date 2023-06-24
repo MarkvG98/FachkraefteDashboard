bea338e63b8e68af8a767f081e14a1d9ea760766
Jannik Hübschmann <jannik@huebschmann-online.de>
2023-06-24 10:38:41 +0200

    Kein Backup, kein Mitleid

--- main/py/connectors.py
+++ main/py/connectors.py
@@ -3,7 +3,7 @@ from requests import Response
 
 class RestApiConnector:
 
-    def getHTTPResponse(self,page: int) -> Response:
+    def getHTTPResponse(self,page: int,postalCode: []) -> Response:
         client_id = "c003a37f-024f-462a-b36d-b001be4cd24a"
         client_secret = "32a39620-32b3-4307-9aa1-511e3d7f48a8"
         url = "https://rest.arbeitsagentur.de/oauth/gettoken_cc"
@@ -26,11 +26,11 @@ class RestApiConnector:
 
         params = {
             "angebotsart": "1",
-            "wo": "Berlin",
-            "umkreis": "200",
+            "wo": postalCode,
+            "umkreis": "0",
             "arbeitszeit": "ho;mj",
-            "page": page,
-            "size": "100",
+            "page": "1",
+            "size": "25",
             "pav": "false"
         }
         response = requests.get(url, headers=headers, params=params)
--- main/py/pandas/dataAnalysisTesting.py
+++ main/py/pandas/dataAnalysisTesting.py
@@ -1,11 +1,11 @@
+import time
+
 from main.py.connectors import RestApiConnector
 import pandas as pd
 import json
 
 con = RestApiConnector()
 
-
-# jobs = con.getHTTPResponse().json()
 
 def set_pandas_display_options() -> None:
     # Ref: https://stackoverflow.com/a/52432757/
@@ -19,21 +19,41 @@ def set_pandas_display_options() -> None:
 set_pandas_display_options()
 
 
-def getDictForDE(pagesToShow: int) -> []:
+def getDictForDE(pagesToShow: int, postalCodes: []) -> []:
     data = []
-    for index in range(pagesToShow):
-        jobsForPage = con.getHTTPResponse(index + 1).json()
-        for entity in jobsForPage["stellenangebote"]:
-            dict = {
-                "beruf": entity.get("beruf", None),
-                "titel": entity.get("titel", None),
-                "refnr": entity.get("refnr", None),
-                "arbeitgeber": entity.get("arbeitgeber", None),
-                "eintrittsdatum": entity.get("eintrittsdatum", None),
-                "entfernung": entity.get("arbeitsort", "").get("entfernung", None),
-                "plz": entity.get("arbeitsort", "").get("plz", None)
-            }
-            data.append(dict)
+    it = 1
+    for psDict in postalCodes:
+        # Fehlervorbeugung, Check ob das returnte Dictionary wirklich nur eine PLZ enthält und ein 5-Stelliger Code ist
+        if len(psDict) == 1 and len(psDict.get('plz')) == 5:
+            ps = psDict.get('plz')
+
+            jobsForPage = con.getHTTPResponse(1, ps).json()
+            #Gibt es Stellenangebote in der Page oder ist die leer?
+            if jobsForPage.get("maxErgebnisse") >= 1:
+                for entity in jobsForPage["stellenangebote"]:
+                    dict = {
+                        "beruf": entity.get("beruf", None),
+                        "titel": entity.get("titel", None),
+                        "refnr": entity.get("refnr", None),
+                        "arbeitgeber": entity.get("arbeitgeber", None),
+                        "eintrittsdatum": entity.get("eintrittsdatum", None),
+                        "entfernung": entity.get("arbeitsort", "").get("entfernung", None),
+                        "plz": entity.get("arbeitsort", "").get("plz", None)
+                    }
+                    data.append(dict)
+                # Ja, das muss hier wirklich stehen, damit die API einen nicht rauswirft.
+            time.sleep(1)
+            print("Ping"+str(it))
+            it+=1
+            if len(data) == 2100:
+                df_codes = pd.DataFrame.from_dict(cityData)
+                df_nach_stellenangeboten = pd.DataFrame.from_dict(data)
+                df_nach_stellenangeboten.sort_values(by=["beruf"])
+                df_nach_stellenangeboten = pd.merge(df_nach_stellenangeboten, df_codes, how='inner', on=["plz", "plz"])
+                dfjson = df_nach_stellenangeboten.to_json(orient="records")
+                file = open("returnData.json")
+                json.write(dfjson)
+
     return data
 
 
@@ -44,23 +64,22 @@ def getPostalData() -> []:
     cityCodes = json.load(f)
     for gemeinde in cityCodes["gemeinden"]:
         dict = {
-            #Rausgenommen weil nicht zuordbar
-            #"id": gemeinde.get("id", None),
+            # Rausgenommen weil nicht zuordbar
+            # "id": gemeinde.get("id", None),
             "plz": gemeinde.get("postleitzahl", None)
         }
-        if(dict not in data):
+        if (dict not in data):
             data.append(dict)
     f.close()
     return data
 
 
-data = getDictForDE(15)
+codes = getPostalData()
+data = getDictForDE(400, codes)
 cityData = getPostalData()
 
 df_codes = pd.DataFrame.from_dict(cityData)
 df_nach_stellenangeboten = pd.DataFrame.from_dict(data)
 df_nach_stellenangeboten.sort_values(by=["beruf"])
-df_nach_stellenangeboten = pd.merge(df_nach_stellenangeboten,df_codes,how='inner',on=["plz","plz"])
+df_nach_stellenangeboten = pd.merge(df_nach_stellenangeboten, df_codes, how='inner', on=["plz", "plz"])
 print(df_nach_stellenangeboten)
-
-
