a1722bf17f0d0ddb12b46f8647b2f12629c30162
mava@vkf-renzel.de <mava@vkf-renzle.de>
2023-07-04 17:01:53 +0200

    c# heatmap

---
+++ main/report/DevExpressC/DXApplication2/DXApplication2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace DXApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapControl1 = new DevExpress.XtraMap.MapControl();
+            this.MapLayer = new DevExpress.XtraMap.ImageLayer();
+            this.wmsDataProvider1 = new DevExpress.XtraMap.WmsDataProvider();
+            this.heatMapLayer = new DevExpress.XtraMap.ImageLayer();
+            this.heatmapProvider1 = new DevExpress.XtraMap.HeatmapProvider();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.CenterPoint = new DevExpress.XtraMap.GeoPoint(50D, 11D);
+            this.mapControl1.Layers.Add(this.MapLayer);
+            this.mapControl1.Layers.Add(this.heatMapLayer);
+            this.mapControl1.Location = new System.Drawing.Point(240, -1);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(593, 533);
+            this.mapControl1.TabIndex = 0;
+            this.mapControl1.ZoomLevel = 5D;
+            this.MapLayer.DataProvider = this.wmsDataProvider1;
+            this.MapLayer.EnableAntialiasing = DevExpress.Utils.DefaultBoolean.True;
+            this.MapLayer.Name = "WmsMap";
+            this.wmsDataProvider1.ActiveLayerName = "TOPO-OSM-WMS";
+            this.wmsDataProvider1.ServerUri = "http://ows.mundialis.de/services/service?";
+            this.heatMapLayer.DataProvider = this.heatmapProvider1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 660);
+            this.Controls.Add(this.mapControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl1;
+        private DevExpress.XtraMap.ImageLayer MapLayer;
+        private DevExpress.XtraMap.WmsDataProvider wmsDataProvider1;
+        private DevExpress.XtraMap.ImageLayer heatMapLayer;
+        private DevExpress.XtraMap.HeatmapProvider heatmapProvider1;
+    }
+}
+
---
+++ main/report/DevExpressC/DXApplication2/DXApplication2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.XtraMap;
+using System;
+using Newtonsoft;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraPrinting.Native.WebClientUIControl;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DXApplication2
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HeatmapDataSourceAdapter adapter = new HeatmapDataSourceAdapter();
+            adapter.Mappings.XCoordinate = "lon"; //The data souce field name that provides x-coordinate
+            adapter.Mappings.YCoordinate = "lat"; //The data sou"rce field name that provides y-coordinate
+            adapter.DataSource = LoadData("C:\\projects\\dashboard\\FachkraefteDashboard\\main\\dump\\TestJason.json");
+
+            HeatmapProvider provider = new HeatmapProvider();
+            provider.PointSource = adapter;
+            provider.Algorithm = new HeatmapDensityBasedAlgorithm { PointRadius = 3 };
+
+            ImageLayer heatmapLayer = new ImageLayer();
+            heatmapLayer.DataProvider = provider;
+            mapControl1.Layers.Add(heatmapLayer);
+        }
+
+        DataTable LoadData(string filepath)
+        {
+            string json = File.ReadAllText(filepath);
+            return (DataTable)JsonConvert.DeserializeObject(json, typeof(DataTable));
+        }
+    }
+}
