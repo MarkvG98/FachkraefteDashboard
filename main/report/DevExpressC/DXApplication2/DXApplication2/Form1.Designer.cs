9d213c00563e8b019550310cf5b269d9878a8e32
mava@vkf-renzel.de <mava@vkf-renzle.de>
2023-07-04 21:42:47 +0200

    Erweiterung C# Map mit Json FIlter

--- main/report/DevExpressC/DXApplication2/DXApplication2/Form1.Designer.cs
+++ main/report/DevExpressC/DXApplication2/DXApplication2/Form1.Designer.cs
@@ -28,42 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraCharts.XYDiagram xyDiagram3 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series5 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series6 = new DevExpress.XtraCharts.Series();
             this.mapControl1 = new DevExpress.XtraMap.MapControl();
             this.MapLayer = new DevExpress.XtraMap.ImageLayer();
             this.wmsDataProvider1 = new DevExpress.XtraMap.WmsDataProvider();
             this.heatMapLayer = new DevExpress.XtraMap.ImageLayer();
             this.heatmapProvider1 = new DevExpress.XtraMap.HeatmapProvider();
+            this.btnFilterTxt = new System.Windows.Forms.Button();
+            this.txtBoxFilter = new System.Windows.Forms.TextBox();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.btnResetFilter = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series6)).BeginInit();
             this.SuspendLayout();
             // 
             // mapControl1
             // 
-            this.mapControl1.CenterPoint = new DevExpress.XtraMap.GeoPoint(50D, 11D);
+            this.mapControl1.CenterPoint = new DevExpress.XtraMap.GeoPoint(50.5D, 10D);
             this.mapControl1.Layers.Add(this.MapLayer);
             this.mapControl1.Layers.Add(this.heatMapLayer);
-            this.mapControl1.Location = new System.Drawing.Point(240, -1);
+            this.mapControl1.Location = new System.Drawing.Point(12, -1);
             this.mapControl1.Name = "mapControl1";
-            this.mapControl1.Size = new System.Drawing.Size(593, 533);
+            this.mapControl1.Size = new System.Drawing.Size(735, 649);
             this.mapControl1.TabIndex = 0;
-            this.mapControl1.ZoomLevel = 5D;
+            this.mapControl1.ZoomLevel = 5.8D;
             this.MapLayer.DataProvider = this.wmsDataProvider1;
             this.MapLayer.EnableAntialiasing = DevExpress.Utils.DefaultBoolean.True;
             this.MapLayer.Name = "WmsMap";
-            this.wmsDataProvider1.ActiveLayerName = "TOPO-OSM-WMS";
+            this.wmsDataProvider1.ActiveLayerName = "OSM-WMS";
             this.wmsDataProvider1.ServerUri = "http://ows.mundialis.de/services/service?";
             this.heatMapLayer.DataProvider = this.heatmapProvider1;
+            // 
+            // btnFilterTxt
+            // 
+            this.btnFilterTxt.Location = new System.Drawing.Point(985, 517);
+            this.btnFilterTxt.Name = "btnFilterTxt";
+            this.btnFilterTxt.Size = new System.Drawing.Size(75, 23);
+            this.btnFilterTxt.TabIndex = 1;
+            this.btnFilterTxt.Text = "Filter";
+            this.btnFilterTxt.UseVisualStyleBackColor = true;
+            this.btnFilterTxt.Click += new System.EventHandler(this.btnFilterTxt_Click);
+            // 
+            // txtBoxFilter
+            // 
+            this.txtBoxFilter.Location = new System.Drawing.Point(881, 478);
+            this.txtBoxFilter.Name = "txtBoxFilter";
+            this.txtBoxFilter.Size = new System.Drawing.Size(312, 23);
+            this.txtBoxFilter.TabIndex = 2;
+            // 
+            // chartControl1
+            // 
+            xyDiagram3.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram3.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram3;
+            this.chartControl1.Legend.LegendID = -1;
+            this.chartControl1.Location = new System.Drawing.Point(753, -1);
+            this.chartControl1.Name = "chartControl1";
+            series5.ArgumentDataMember = "XValue";
+            series5.Name = "Series 1";
+            series5.SeriesID = 0;
+            series5.ValueDataMembersSerializable = "YValue";
+            series6.ArgumentDataMember = "XValue";
+            series6.Name = "Series 2";
+            series6.SeriesID = 1;
+            series6.ValueDataMembersSerializable = "XYValue";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series5,
+        series6};
+            this.chartControl1.Size = new System.Drawing.Size(649, 445);
+            this.chartControl1.TabIndex = 3;
+            this.chartControl1.ObjectSelected += new DevExpress.XtraCharts.HotTrackEventHandler(this.chartControl1_ObjectSelected);
+            // 
+            // btnResetFilter
+            // 
+            this.btnResetFilter.Location = new System.Drawing.Point(1108, 517);
+            this.btnResetFilter.Name = "btnResetFilter";
+            this.btnResetFilter.Size = new System.Drawing.Size(120, 23);
+            this.btnResetFilter.TabIndex = 4;
+            this.btnResetFilter.Text = "Reset Filter";
+            this.btnResetFilter.UseVisualStyleBackColor = true;
+            this.btnResetFilter.Click += new System.EventHandler(this.btnResetFilter_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 660);
+            this.ClientSize = new System.Drawing.Size(1918, 1040);
+            this.Controls.Add(this.btnResetFilter);
+            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.txtBoxFilter);
+            this.Controls.Add(this.btnFilterTxt);
             this.Controls.Add(this.mapControl1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +145,10 @@
         private DevExpress.XtraMap.WmsDataProvider wmsDataProvider1;
         private DevExpress.XtraMap.ImageLayer heatMapLayer;
         private DevExpress.XtraMap.HeatmapProvider heatmapProvider1;
+        private System.Windows.Forms.Button btnFilterTxt;
+        private System.Windows.Forms.TextBox txtBoxFilter;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.Button btnResetFilter;
     }
 }
 
--- main/report/DevExpressC/DXApplication2/DXApplication2/Form1.cs
+++ main/report/DevExpressC/DXApplication2/DXApplication2/Form1.cs
@@ -11,32 +11,114 @@ using System.Windows.Forms;
 using DevExpress.XtraPrinting.Native.WebClientUIControl;
 using Newtonsoft.Json;
 using System.IO;
+using DevExpress.XtraBars;
+using DevExpress.XtraCharts;
+using System.Diagnostics;
 
 namespace DXApplication2
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        HeatmapDataSourceAdapter adapter;
+        HeatmapProvider provider;
         public Form1()
         {
             InitializeComponent();
-            HeatmapDataSourceAdapter adapter = new HeatmapDataSourceAdapter();
-            adapter.Mappings.XCoordinate = "lon"; //The data souce field name that provides x-coordinate
-            adapter.Mappings.YCoordinate = "lat"; //The data sou"rce field name that provides y-coordinate
-            adapter.DataSource = LoadData("C:\\projects\\dashboard\\FachkraefteDashboard\\main\\dump\\TestJason.json");
+            adapter = new HeatmapDataSourceAdapter();
+            adapter.Mappings.XCoordinate = "lon"; //lon
+            adapter.Mappings.YCoordinate = "lat"; //lat
 
-            HeatmapProvider provider = new HeatmapProvider();
+            provider = new HeatmapProvider();
             provider.PointSource = adapter;
             provider.Algorithm = new HeatmapDensityBasedAlgorithm { PointRadius = 3 };
 
             ImageLayer heatmapLayer = new ImageLayer();
             heatmapLayer.DataProvider = provider;
             mapControl1.Layers.Add(heatmapLayer);
+
+
+            ChoroplethColorizer colorizer = new ChoroplethColorizer();
+            colorizer.RangeStops.AddRange(new double[] { 0.1, 0.3, 0.5, 0.7, 1 });
+            colorizer.ColorItems.Add(new ColorizerColorItem(Color.Green));
+            colorizer.ColorItems.Add(new ColorizerColorItem(Color.YellowGreen));
+            colorizer.ColorItems.Add(new ColorizerColorItem(Color.Yellow));
+            colorizer.ColorItems.Add(new ColorizerColorItem(Color.Orange));
+            colorizer.ColorItems.Add(new ColorizerColorItem(Color.Red));
+            colorizer.ApproximateColors = true;
+
+            provider.Colorizer = colorizer;
         }
 
-        DataTable LoadData(string filepath)
+        private List<MainSource> MainJobDataSource = new List<MainSource>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MainJobDataSource = LoadData("C:\\projects\\dashboard\\FachkraefteDashboard\\main\\dump\\TestJason.json");
+            adapter.DataSource = MainJobDataSource;
+
+            var grouped = MainJobDataSource.GroupBy(g => g.arbeitgeber).OrderByDescending(
+                o => o.Count()).Take(5).Select(s => new ChartData()
+                {
+                    XValue = s.Key,
+                    YValue = s.Count()
+                }).ToList();
+
+            chartControl1.DataSource = grouped;
+        }
+
+        public class ChartData
+        {
+            public string XValue { get; set; }
+            public int YValue { get; set; }
+
+            public int XYValue { get; set; }
+        }
+
+        private void btnFilterTxt_Click(object sender, EventArgs e)
+        {
+            string filter = txtBoxFilter.Text;
+            FilterHeatmap(filter);
+        }
+
+        private void FilterHeatmap(string filter)
+        {
+            var filteredList = MainJobDataSource.Where(s => s.arbeitgeber == filter).ToList();
+            var densityAlg = provider.Algorithm as HeatmapDensityBasedAlgorithm;
+            densityAlg.PointRadius = 10;
+            adapter.DataSource = filteredList;
+        }
+
+        List<MainSource> LoadData(string filepath)
         {
             string json = File.ReadAllText(filepath);
-            return (DataTable)JsonConvert.DeserializeObject(json, typeof(DataTable));
+            return JsonConvert.DeserializeObject<List<MainSource>>(json);
+        }
+
+        public class MainSource
+        {
+            public string beruf;
+            public string titel;
+            public string refnr;
+            public string arbeitgeber;
+            public DateTime eintrittsdatum;
+            public string entfernung;
+            public string PLZ;
+            public double lat { get; set; }
+            public double lon { get; set; }
+
+            public double Value => 1;
+        }
+
+        private void chartControl1_ObjectSelected(object sender, HotTrackEventArgs e)
+        {
+            if (e.HitInfo.SeriesPoint != null)
+            {
+                FilterHeatmap(e.HitInfo.SeriesPoint.Argument);
+            }
+        }
+
+        private void btnResetFilter_Click(object sender, EventArgs e)
+        {
+            adapter.DataSource = MainJobDataSource;
         }
     }
 }
